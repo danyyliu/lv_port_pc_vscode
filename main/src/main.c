5ccc86087f3debf0bac7751d868ab0541b2abf26
Dany Liu <dany.yang.liu@email.com>
2024-05-13 23:43:07 +0800

    feat(opengles): Add opengl library for building

--- main/src/main.c
+++ main/src/main.c
@@ -96,6 +96,21 @@ static lv_display_t * hal_init(int32_t w, int32_t h)
 
   lv_group_set_default(lv_group_create());
 
+#if 1
+  lv_display_t * disp = lv_opengles_window_create(w, h);
+
+  lv_indev_t * mouse = lv_opengles_mouse_create();
+  lv_indev_set_group(mouse, lv_group_get_default());
+  lv_indev_set_display(mouse, disp);
+  lv_display_set_default(disp);
+
+  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+  lv_obj_t * cursor_obj;
+  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
+  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
+
+#else
   lv_display_t * disp = lv_sdl_window_create(w, h);
 
   lv_indev_t * mouse = lv_sdl_mouse_create();
@@ -116,6 +131,7 @@ static lv_display_t * hal_init(int32_t w, int32_t h)
   lv_indev_t * kb = lv_sdl_keyboard_create();
   lv_indev_set_display(kb, disp);
   lv_indev_set_group(kb, lv_group_get_default());
+#endif
 
   return disp;
 }
